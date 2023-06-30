2c9f5adb3b36a1b156919b37742e8eb0714790c8
Ckm54 <mutaic84@gmail.com>
2023-06-30 16:07:22 +0300

    add error handling

---
+++ src/controllers/error.controller.ts
@@ -0,0 +1,19 @@
+import { GraphQLError } from "graphql";
+
+const handleCastError = (error: any) => {
+  const message = `Invalid ${error.path}: ${error.value}`;
+  throw new GraphQLError(message);
+};
+
+const handleValidationError = (error: any) => {
+  const message = Object.values(error.errors).map((el: any) => el.message);
+  throw new GraphQLError(`Invalid input: ${message.join(", ")}`);
+};
+
+const errorHandler = (err: any) => {
+  if (err.name === "CastError") handleCastError(err);
+  if (err.name === "ValidationError") handleValidationError(err);
+  throw err;
+};
+
+export default errorHandler;
